5f908ca92cfc06df665a1684333880b179044665
Meganathan <meganathan212003@gmail.com>
2023-04-12 18:43:37 +0530

    feat: index search filter added

--- main.js
+++ main.js
@@ -1,12 +1,62 @@
-if(localStorage.getItem("userdata")){
-    return;
-}
-else{
-    localStorage.setItem()
+if (localStorage.getItem("userslist")) {
+
 }
 
 
+else {
+    localStorage.setItem("userslist", JSON.stringify([
+        {
+            confirm_password
+                :
+                "Meganathan212003@gmail.com",
+            email
+                :
+                "meganathan212003@gmail.com",
+            name
+                :
+                "meganathan",
+            password
+                :
+                "Meganathan212003@gmail.com",
+            user_name
+                :
+                "dinesh"
+        }
+
+    ]));
+}
+
+if (localStorage.getItem("Book_details")) {
+
+}
+
+else {
+    localStorage.setItem("Book_details", JSON.stringify([
+        {
+
+            DiscountPrice
+                :
+                "200",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/617UBjNbf9L.jpg",
+            bookName
+                :
+                "ponniyin selvan book for tamil",
+            bookid
+                :
+                27,
+            originalPrice
+                :
+                "100",
+            rating
+                :
+                ""
+        }
+    ]));
+}
 
 
- 
+
+
 
