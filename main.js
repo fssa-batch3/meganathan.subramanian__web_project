f0c66deb3567a6f65d5ff5c947f51ecdc1c91234
Meganathan <meganathan212003@gmail.com>
2023-04-12 18:52:13 +0530

    fix: alter the main js code

--- main.js
+++ main.js
@@ -50,7 +50,108 @@ else {
             rating
                 :
                 ""
+        }, {
+            DiscountPrice
+                :
+                "200",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/516VyKCKi-L.jpg",
+            bookName
+                :
+                "Eat that frog Tamil edition",
+            bookid
+                :
+                19,
+            originalPrice
+                :
+                "100",
+            rating
+                :
+                ""
+        },
+        {
+            DiscountPrice
+                :
+                "200",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/71H8-WDVR9L.jpg",
+            bookName
+                :
+                " Happy is the Goal Tamil Edition Book",
+            bookid
+                :
+                1,
+            originalPrice
+                :
+                "100",
+            rating
+                :
+                ""
+        },
+        {
+            DiscountPrice
+                :
+                "300",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/51jpuH++30L._SX318_BO1,204,203,200_.jpg",
+            bookName
+                :
+                " Rich dad Poor Dad in Tamil edition",
+            bookid
+                :
+                57,
+            originalPrice
+                :
+                "100",
+            rating
+                :
+                ""
+
+        },
+        {
+            DiscountPrice
+                :
+                "500",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/712H8OZbswS.jpg",
+            bookName
+                :
+                "Chankya nithi tamil edition book",
+            bookid
+                :
+                73
+originalPrice
+                :
+                "100",
+            rating
+                :
+                ""
+        },
+        {
+            DiscountPrice
+                :
+                "700",
+            bookImage
+                :
+                "https://m.media-amazon.com/images/I/71Rcjb+1yLL.jpg",
+            bookName
+                :
+                "mystry",
+            bookid
+                :
+                44,
+            originalPrice
+                :
+                "180",
+            rating
+                :
+                ""
         }
+
     ]));
 }
 
